a07ee942b2ddf502ca29a2e3e484c42d30446f8c
Marcelo Pugliesi <34604483+masp42@users.noreply.github.com>
2025-04-19 22:26:24 -0300

    Create init.sql
    official image with an .sql init file.

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS meubanco;
+
+USE meubanco;
+
+CREATE TABLE dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
